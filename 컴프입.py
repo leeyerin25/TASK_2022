151fcfbe0ca6f0add3fa68f169b354a163f4c1eb
yerin lee <101498705+leeyerin25@users.noreply.github.com>
2022-06-08 11:53:51 +0900

    Update 컴프입.py

--- 컴프입.py
+++ 컴프입.py
@@ -48,3 +48,53 @@ while dec > 0: #정수면 계속 돌거야
 ls.reverse()
 ls = "".join(map(str, ls))
 print(f"{dec_2}에 대한 2진수: {ls}")
+
+
+
+문제 : 
+첨부 파일 gettysburg.txt 파일을 사용하여 키(key)가 단어이고 값(value)은 단어가 나타난 줄 번호 리스트인 딕셔너리를 만들고, 
+키(key)인 단어의 알파벳 순으로 파일 (data/word_frequency.dat’)에 저장하는 프로그램을 작성하시오.
+
+
+# 딕셔너리 추가하는법!! 무에서 유를 창조
+words_dict = {}
+words_dict['word'] = [2]
+print(words_dict)
+
+
+def read_file(filename):
+    special_ch = '~`@#$%^.&*,()-_+="<>][;:?' + "'"
+    words_dict = {}
+    line_num = 0
+    with open ('C:/Users/yerin/pythonProject/gettysburg.txt','r') as infile:
+        for line in infile:
+            line_num += 1
+            for ch in special_ch:
+                line = line.replace(ch,"")
+            words_list = line.split()
+            for word in words_list:  # 하나의 line_num 안에서 계속 돔
+                word = word.lower()
+                if word in words_dict:  # 처음엔 words_dict 는 정의된게 없고
+                    words_dict[word].append(line_num)
+                else:           # 이부분에서 words_dict 의 내용이 생성이됨
+                   words_dict[word] = [line_num]
+
+    return words_dict #모든문장을 한번에 받기위해 줄을 with 과 맞춰준다.
+
+def save_line_num(filename, words_dict):
+    with open(filename, 'w') as outfile:
+        for word in sorted(words_dict): #오름차순으로 빼서 정리함
+            outfile.write(f'{word:12s}:')  # 단어먼저써지고
+            for line_num in words_dict[word]:
+                outfile.write(f'{line_num}')  # 그옆에 이어서 라인넘버써짐 , (여러개면 여러개써짐)
+            outfile.write('\n')  #그리고 줄바꿈
+
+#1. def read_file => 각 line_number 안에서 words_dict 를 반환  EX) words_dict[word] = [line_num]
+#2. def save_line_num => 그 words_dict 를 가지고 for문을 이용해 한줄한줄 글로 작성.
+
+if __name__=='__main__':
+    input_file = 'C:/Users/yerin/pythonProject/gettysburg.txt'
+    output_file = 'C:/Users/yerin/pythonProject/word_frequency_2.dat'
+    words_d = read_file(input_file)
+    save_line_num(output_file,words_d)
+
